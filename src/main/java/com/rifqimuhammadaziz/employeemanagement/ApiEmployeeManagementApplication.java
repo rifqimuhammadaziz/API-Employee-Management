59ee7fc036cd84ba27cb6769c2978521719db21d
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2022-09-30 08:32:30 +0700

    create spring application & setup database connection

---
+++ src/main/java/com/rifqimuhammadaziz/employeemanagement/ApiEmployeeManagementApplication.java
@@ -0,0 +1,13 @@
+package com.rifqimuhammadaziz.employeemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiEmployeeManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiEmployeeManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rifqimuhammadaziz/employeemanagement/ApiEmployeeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rifqimuhammadaziz.employeemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiEmployeeManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
